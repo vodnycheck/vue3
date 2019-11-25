357c9109f1b49c471b153f7fc58dbe2d4d77fd72
Artem Vodianytskyi <vodianitsky@gmail.com>
2019-11-25 21:20:32 +0200

    grooming

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import Vue from 'vue';
+import Vue from 'vue'
 import App from './app/app.js'
 
-console.log(1);
-
 window.addEventListener('DOMContentLoaded', () => {
-    console.log(3);
     Vue.createApp().mount(App, '#app')
 })
